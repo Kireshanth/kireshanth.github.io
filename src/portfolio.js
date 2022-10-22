5e489aa03ea5b52ac8362f041ec2097a80714d47
Kireshanth <kireshantht@gmail.com>
2022-10-22 19:23:17 -0400

    updated href

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://kireshanth.github.io/cleanfolio',
+  homepage: 'https://kireshanth.github.io/',
   title: '</Kireshanth>',
 }
 
